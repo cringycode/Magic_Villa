e98410ec5d947ac2071b3a225111fde3a385bbab
kourosh karimi <karimikorosh2000@gmail.com>
2024-05-03 16:02:45 +0330

    Post API and Validations.

--- Magic_Villa_VillaAPI/Controller/VillaAPIController.cs
+++ Magic_Villa_VillaAPI/Controller/VillaAPIController.cs
@@ -8,16 +8,71 @@ namespace Magic_Villa_VillaAPI.Controller
     [ApiController]
     public class VillaAPIController : ControllerBase
     {
+        #region GET ALL
+
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return VillaStore.villaList;
+            return Ok(VillaStore.villaList);
         }
 
-        [HttpGet("id:int")]
-        public VillaDTO GetVilla(int id)
+        #endregion
+
+        #region GET
+
+        [HttpGet("id:int", Name = "GetVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
         }
+
+        #endregion
+
+        #region CREATE
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
+        {
+            if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa Already Exists!");
+                return BadRequest(ModelState);
+            }
+
+            if (villaDTO is null)
+            {
+                return BadRequest(villaDTO);
+            }
+
+            if (villaDTO.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+            VillaStore.villaList.Add(villaDTO);
+
+            return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);
+        }
+
+        #endregion
     }
 }
--- Magic_Villa_VillaAPI/Models/Dto/VillaDTO.cs
+++ Magic_Villa_VillaAPI/Models/Dto/VillaDTO.cs
@@ -1,8 +1,9 @@
-﻿namespace Magic_Villa_VillaAPI.Models.Dto;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Magic_Villa_VillaAPI.Models.Dto;
 
 public class VillaDTO
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    
+    [Required] [MaxLength(30)] public string Name { get; set; }
 }
