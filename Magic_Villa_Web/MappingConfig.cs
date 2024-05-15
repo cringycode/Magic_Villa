47e949f6dd5206eaa557a273926ecae05374c4e4
kourosh karimi <karimikorosh2000@gmail.com>
2024-05-15 16:20:55 +0330

    Base Service (MVC Project)

---
+++ Magic_Villa_Web/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Magic_Villa_Web.Models.Dto;
+using Magic_Villa_Web.Models;
+
+namespace Magic_Villa_Web;
+
+public class MappingConfig : Profile
+{
+    public MappingConfig()
+    {
+        CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+        CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+        CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+        CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+    }
+}
